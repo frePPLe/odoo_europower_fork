ae88c503c0e1b403f65cff7dc771026ec88365c5
Johan De Taeye <jdetaeye@frepple.com>
2022-04-26 12:39:48 +0200

    [14.0] [FIX] Improved version of the previous commit

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -416,7 +416,7 @@ class exporter(object):
         """
         self.map_customers = {}
         m = self.env["res.partner"]
-        recs = m.search([])
+        recs = m.search([("is_company", "=", True)])
         if recs:
             yield "<!-- customers -->\n"
             yield "<customers>\n"
@@ -1136,6 +1136,19 @@ class exporter(object):
             j = so[i["order_id"][0]]
             location = j["warehouse_id"][1]
             customer = self.map_customers.get(j["partner_id"][0], None)
+            if not customer:
+                # The customer may be an individual.
+                # We check whether his/her company is in the map.
+                for c in (
+                    self.env["res.partner"]
+                    .browse([j["partner_id"][0]])
+                    .read(["commercial_partner_id"])
+                ):
+                    customer = self.map_customers.get(
+                        c["commercial_partner_id"][0], None
+                    )
+                    if customer:
+                        break
             if not customer or not location or not product:
                 # Not interested in this sales order...
                 continue
