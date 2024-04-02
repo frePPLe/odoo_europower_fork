8225451490b980c23e11be28522c4620b7e846f1
Kyan Machiels <kyan@weltschmerz.dev>
2024-04-02 10:25:20 +0200

    1. Added a button on sales orders that will quote all the products in a sales order.
    - If all products can be quote'd, the delivery date will be changed to the latest date of all the products.
    - If not all products can be quote'd we dont' automatically change the date.
    - In any case we log the quote output in the chatter.
    
    2. Added a standalone application where you can quote a single product. And it will return a detailed explanation of that product's quotation.

---
+++ frepple/models/quote.py
@@ -0,0 +1,179 @@
+from odoo import fields, models, api, exceptions
+import time
+import jwt
+import requests
+from datetime import datetime
+
+class Quote(models.Model):
+    _name = "frepple.quote"
+    _description = "Frepple Quote's"
+
+    product_id = fields.Many2one("product.template", string="Product", required=True)
+    warehouse_id = fields.Many2one("stock.warehouse", string="Warehouse", required=True)
+    quantity = fields.Integer(string="Quantity", default=1, required=True)
+    minimum_shipment = fields.Integer(string="Minimum Shipment", required=True)
+    due_date = fields.Datetime(string="Due Date", default=lambda _: datetime.today(), required=True)
+    maximum_lateness = fields.Integer(string="Maximum Lateness (in days)", default=1000, required=True)
+    quote = fields.Html(string="Quote Info", readonly=True)
+    detailed_quote = fields.Html(string="Detailed Quote Info", readonly=True)
+
+    @api.depends("quantity")
+    def _compute_minimum_shipment(self):
+        for quote in self:
+            if quote.quantity:
+                quote.minimum_shipment = quote.quantity
+            else:
+                quote.minimum_shipment = 0
+
+    def _generate_html(self, frepple_json):
+        html = '''
+        <div style="font-family: \'Segoe UI\', Tahoma, Geneva, Verdana, sans-serif; padding: 20px; background-color: #f4f4f4; color: #333;">'''
+
+        if frepple_json["demands"][0].get("pegging"):
+            html = html + '''
+            <div style="margin-bottom: 40px;">
+                <h2 style="margin: 0 0 20px 0; padding-bottom: 10px; border-bottom: 3px solid #3498db; color: #3498db;">Category: Operations</h2>
+                <ul style="list-style-type: none; padding: 0;">
+        '''
+            for i, operation in enumerate(frepple_json["demands"][0]["pegging"]):
+                if i != 0 and operation["level"] <= frepple_json["demands"][0]["pegging"][i - 1]["level"]:
+                    for _ in range(frepple_json["demands"][0]["pegging"][i - 1]["level"] - operation["level"] + 1):
+                        html = html + '''
+                            </div>
+        '''
+                if i == 0:
+                    html = html + f'''
+                        <li style="background-color: #fff; margin-bottom: 5px; padding: 15px; border-left: 5px solid #3498db;">
+                            {operation["level"]} {operation["operationplan"]["operation"]["name"]}
+                            <br />Quantity: {operation["operationplan"]["quantity"]}
+                            <br />Start Date: {operation["operationplan"]["start"]}
+                            <br />End Date: {operation["operationplan"]["end"]}
+        ''' 
+                
+                else:
+                    if operation["level"] == 0:
+                        html = html + f'''
+                        </li>
+                        <li style="background-color: #fff; margin-bottom: 5px; padding: 15px; border-left: 5px solid #3498db;">
+                            {operation["level"]} {operation["operationplan"]["operation"]["name"]}
+                            <br />Quantity: {operation["operationplan"]["quantity"]}
+                            <br />Start Date: {operation["operationplan"]["start"]}
+                            <br />End Date: {operation["operationplan"]["end"]}
+        '''
+                    else:
+                        html = html + f'''
+                        <div style="margin-top: 10px; padding-left: 20px; border-left: 2px dashed #bdc3c7;">
+                            <strong>Sub-operation: {operation["level"]}</strong>
+                            <br>{operation["operationplan"]["operation"]["name"]}
+                            <br>Quantity: {operation["operationplan"]["quantity"]}
+                            <br>Start Date: {operation["operationplan"]["start"]}
+                            <br>End Date: {operation["operationplan"]["end"]}
+        '''
+
+            for i in range(frepple_json["demands"][0]["pegging"][len(frepple_json["demands"][0]["pegging"]) - 1]["level"]):
+                html = html + '</div>'
+            html = html + '''
+                    </li>
+                </ul>
+            </div>
+        '''
+
+        if frepple_json["demands"][0].get("problems"):
+            html = html + '''
+            <div style="margin-bottom: 40px;">
+                <h2 style="margin: 0 0 20px 0; padding-bottom: 10px; border-bottom: 3px solid #e74c3c; color: #e74c3c;">Category: Problems</h2>
+                <ul style="list-style-type: none; padding: 0;">
+        '''
+            for problem in frepple_json["demands"][0]["problems"]:
+                html = html + f'''
+                    <li style="background-color: #fff; margin-bottom: 5px; padding: 15px; border-left: 5px solid #e74c3c;">
+                    {problem["description"]}
+                    </li>
+        '''
+            html = html + '''
+                </ul>
+            </div>
+        '''
+
+        if frepple_json["demands"][0].get("constraints"):
+            html = html + '''
+            <div style="margin-bottom: 40px;">
+                <h2 style="margin: 0 0 20px 0; padding-bottom: 10px; border-bottom: 3px solid #e74c3c; color: #e74c3c;">Category: Constraints</h2>
+                <ul style="list-style-type: none; padding: 0;">
+        '''
+            for constraint in frepple_json["demands"][0]["constraints"]:
+                html = html + f'''
+                    <li style="background-color: #fff; margin-bottom: 5px; padding: 15px; border-left: 5px solid #e74c3c;">
+                    {constraint["description"]}
+                    </li>
+        '''
+            html = html + '''
+                </ul>
+            </div>
+        '''
+
+        html + html + '''
+        </div>
+        '''
+        return html
+
+
+    def fetch_quote(self):
+        for quote in self:
+            if quote.product_id and quote.warehouse_id and quote.quantity:
+
+                request_body = {"demands": []}
+                product_name = "[" + str(quote.product_id.default_code) + "] " + str(quote.product_id.name)
+                request_body["demands"].append({
+                    "name": str(quote.product_id.default_code) + " - " + product_name,
+                    "quantity": quote.quantity,
+                    "description": "",
+                    "due": quote.due_date.strftime("%Y-%m-%dT%H:%M:%S"),
+                    "item": {"name": product_name},
+                    "location": {"name": str(quote.warehouse_id.name)},
+                    "customer": {"name": "N/A"},
+                    "minshipment": quote.minimum_shipment,
+                    "maxlateness": quote.maximum_lateness * 86400, 
+                    "priority": 20
+                })
+
+                encode_params = dict(
+                    exp=round(time.time()) + 600, user=quote.env.user.login
+                )
+                user_company_webtoken = quote.env.user.company_id.webtoken_key
+                if not user_company_webtoken:
+                    raise exceptions.UserError("FrePPLe company web token not configured")
+                
+                base_url = quote.env.user.company_id.frepple_server
+                if not base_url:
+                    raise exceptions.UserError("frePPLe web server not configured")
+
+                webtoken = jwt.encode(encode_params, user_company_webtoken, algorithm="HS256")
+                if not isinstance(webtoken, str):
+                    webtoken = webtoken.decode("ascii")
+
+                # -----[ PERFORM THE REQUEST ]-----
+                
+                headers = {
+                    'Authorization': 'Bearer ' + str(webtoken),
+                    'Content-Type': 'application/json'
+                }
+                frepple_response = requests.post(base_url + "quote/inquiry/", headers=headers, json=request_body)
+                response_status_code = frepple_response.status_code
+
+                # Success = 200
+                # Unauthorized = 401
+                if response_status_code == 401:
+                    raise exceptions.UserError("User is not authorized to use FrePPLe")
+                
+                response_json = frepple_response.json()
+
+                quote.detailed_quote = self._generate_html(response_json)
+                try:
+                    quote.quote = "<ul><li>End Date: " + str(response_json["demands"][0]["pegging"][0]["operationplan"]["end"].split("T")[0]) + '</li></ul>'
+                except Exception as e:
+                    print(e)
+                    quote.quote = 'N/A'
+            else:
+                quote.detailed_quote = "Please fill in all the required fields to receive a quote"
+                quote.quote = "Please fill in all the required fields to receive a quote"
--- frepple/models/sale_order.py
+++ frepple/models/sale_order.py
@@ -1,6 +1,10 @@
 import os
 import logging
-from odoo import models, api, fields
+from odoo import models, api, fields, exceptions
+import requests
+import jwt
+import time
+import datetime
 
 _logger = logging.getLogger(__name__)
 
@@ -9,6 +13,94 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     frepple_write_date = fields.Datetime(string='Write Date (frePPLe)',compute='_compute_frepple_write_date', store=True)
+
+    def action_frepple_quote(self):
+        for sale_order in self:
+            
+            # -----[ BUILD THE REQUEST BODY ]-----
+            request_body = {"demands": []}
+            for line in sale_order.order_line:
+                if line.product_id.type == "product":
+                    product_name = "[" + str(line.product_id.default_code) + "] " + str(line.product_id.name)
+                    request_body["demands"].append({
+                        "name": str(sale_order.name) + " - " + product_name,
+                        "quantity": int(line.product_uom_qty),
+                        "description": "",
+                        "due": "2024-03-19T00:00:00",
+                        "item": {"name": product_name},
+                        "location": {"name": str(sale_order.warehouse_id.name)},
+                        "customer": {"name": str(sale_order.partner_shipping_id.name) + " " + str(sale_order.partner_shipping_id.id)},
+                        "minshipment": int(line.product_uom_qty), # Minimum shipment = Per how many do you want to ship | Zelfde als quantity in the knop
+                        "maxlateness": 86400000, #  Binnen x aantal seconden moet ik het hebben | Niet Belangrijk dus staat op 1000 dagen
+                        "priority": 20 # Niet belangrijk, ik neem info over van wat de quote tool doet.
+                    })
+                
+            # -----[ CREATE AUTH TOKEN ]-----
+            encode_params = dict(
+                exp=round(time.time()) + 600, user=sale_order.env.user.login
+            )
+            user_company_webtoken = sale_order.env.user.company_id.webtoken_key
+            if not user_company_webtoken:
+                raise exceptions.UserError("FrePPLe company web token not configured")
+            
+            base_url = sale_order.env.user.company_id.frepple_server
+            if not base_url:
+                raise exceptions.UserError("frePPLe web server not configured")
+            
+            webtoken = jwt.encode(encode_params, user_company_webtoken, algorithm="HS256")
+            if not isinstance(webtoken, str):
+                webtoken = webtoken.decode("ascii")
+
+
+            # -----[ PERFORM THE REQUEST ]-----
+            
+            headers = {
+                'Authorization': 'Bearer ' + str(webtoken),
+                'Content-Type': 'application/json'
+            }
+            
+
+            frepple_response = requests.post(base_url + "quote/inquiry/", headers=headers, json=request_body)
+            response_status_code = frepple_response.status_code
+            # Success = 200
+            # Unauthorized = 401
+            if response_status_code == 401:
+                raise exceptions.UserError("User is not authorized to use FrePPLe")
+
+            response_json = frepple_response.json()
+            # -----[ CONSTRUCT THE NOTE BODY ]-----
+            # prepare finding the furthest date of all products
+            furthest_end_date = None
+
+            note_body = '<p><font style="font-size: 18px;">--- FrePPle Quote ---</font></p><ul>'
+
+            has_na = False
+
+            for demand in response_json["demands"]:
+                product_name = demand["item"]["name"]
+                try:
+                    end_date = datetime.datetime.strptime(demand["pegging"][0]["operationplan"]["end"].split("T")[0], "%Y-%m-%d")
+                    if furthest_end_date is None or end_date > furthest_end_date:
+                        furthest_end_date = end_date
+                    end_date = end_date.strftime("%Y-%m-%d")
+                except Exception as e:
+                    has_na = True
+                    end_date = "N/A"
+
+                note_body = note_body + "<li><p>" + product_name + " => <b>" + str(end_date) + "</b></p></li>" 
+
+            # -----[ SEND THE NOTE ]-----
+            note_body = note_body + "</ul>"
+            sale_order.message_post(body=note_body)
+
+            # -----[ UPDATE THE DELIVERY DATE ]-----
+            
+            # E-POWER CUSTOMIZATION
+            # if furthest_end_date and sale_order.commitment_date != furthest_end_date:
+            #     sale_order.commitment_date = furthest_end_date
+
+            if furthest_end_date and sale_order.xx_requested_delivery_date != furthest_end_date and not has_na:
+                sale_order.xx_requested_delivery_date = furthest_end_date
 
     @api.depends('order_line.frepple_write_date')
     def _compute_frepple_write_date(self):
